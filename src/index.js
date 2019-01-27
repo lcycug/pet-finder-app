7db647f03dd0cb4ad03cfdc741279633d1a9ab51
Jason Lee <lcycug@live.com>
2019-01-27 13:11:58 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { render } from "react-dom"
-import App from './component/App'
+import React from "react";
+import { render } from "react-dom";
+import App from "./component/App";
+import "./stylesheet/App.css";
 
-render(<App/>, document.getElementById("root"))
+render(<App />, document.getElementById("root"));
